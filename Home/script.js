eb38b48452aade69cf624c3dee8c47f43f5c672d
rahul-yadav695 <officialrahulyadav961@gmail.com>
2025-03-17 12:55:53 +0530

    edit the code

--- Home/script.js
+++ Home/script.js
@@ -1,191 +1,272 @@
-function isUserAuthenticated() {
+
+
+
+function isuserauthicated() {
     const myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFhZGl0eWFrdW1hd2F0MTExQGdtYWlsLmNvbSIsImlhdCI6MTc0MDM3NDYxNSwiZXhwIjoxNzQwNDYxMDE1fQ.nSDr9ZRIp1z1-a1W0OBXdNPB6NMFUVjNWbZGZvR4uz4");
+    myHeaders.append("Authorization", "Bearer sa d,jnsaudsbsaxudnfbdmbcudfcia,bufvdkshkaihfbjdudshfkjdsfkdsakdsakmxidahfufdjf");
+  
     const requestOptions = {
-        method: "GET",
-        headers: myHeaders,
-        redirect: "follow"
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow"
     };
+  
     fetch("https://admin-dev-backend.edvice.in/ping", requestOptions)
-        .then((response) => response.json())
-        .then((result) => {
-            // if (result.success) {
-            let body = document.getElementsByTagName("body");
-            body[0].children[0].remove();
-            // } else {
-            // window.location.href = '/HTML/signUp.html'
-            // console.log(result.error.explanation)
-            // }
-        })
-        .catch((error) => console.error(error));
-}
-window.onload = isUserAuthenticated;
-let datas = [
+      .then((response) => response.json())
+      .then((result) => {
+        // if(result.success){
+        let body = document.getElementsByTagName("body")
+        body[0].children[0].remove();
+        body[0].style.overflowY = "scroll"
+        // }else{
+        //     window.location.href="/sign-in-pages/signin.html"
+        //     console.log(result.error.explanation);
+        // }
+      })
+      .catch((error) => {
+        console.log(error)
+  
+      });
+  }
+  isuserauthicated();
+  
+  
+//   let datas = [
+//     {
+//         images: "./images/Images.png",
+//         p1: "Syltherine",
+//         p2: "Stylish cafe chair",
+//         p3: `Rp 2.500.000 <span class="opacity-50"><del> Rp 3.500.000</del></span>`,
+//         btn: "Add to cart",
+//         p4: "Share",
+//         p5: "Compare",
+//         p6: "Like"
+//     },
+//     {
+//         images: "./images/Images (3).png",
+//         p1: "Leviosa",
+//         p2: "Stylish cafe chair",
+//         p3: "Rp 2.500.000",
+//         btn: "Add to cart",
+//         p4: "Share",
+//         p5: "Compare",
+//         p6: "Like"
+//     },
+//     {
+//         images: "./images/Images (1).png",
+//         p1: "Lolito",
+//         p2: "Luxury big sofa",
+//         p3: `Rp 7.000.000 <span class="opacity-50"><del> Rp 14.000.000</del></span>`,
+//         btn: "Add to cart",
+//         p4: "Share",
+//         p5: "Compare",
+//         p6: "Like"
+//     },
+//     {
+//         images: "./images/Images (2).png",
+//         p1: "Respira",
+//         p2: "Outdoor bar table and stool",
+//         p3: "Rp 500.000",
+//         btn: "Add to cart",
+//         p4: "Share",
+//         p5: "Compare",
+//         p6: "Like"
+//     },
+//     {
+//         images: "./images/Images (4).png",
+//         p1: "Grifo",
+//         p2: "Night lamp",
+//         p3: "Rp 1.500.000",
+//         btn: "Add to cart",
+//         p4: "Share",
+//         p5: "Compare",
+//         p6: "Like"
+//     },
+//     {
+//         images: "./images/Images (2).png",
+//         p1: "Muggo",
+//         p2: "Small mug",
+//         p3: "Rp 150.000",
+//         btn: "Add to cart",
+//         p4: "Share",
+//         p5: "Compare",
+//         p6: "Like"
+//     },
+//     {
+//         images: "./images/Images.png",
+//         p1: "Pingky",
+//         p2: "Cute bed set",
+//         p3: `Rp 7.000.000 <span class="opacity-50"><del>Rp 14.000.000</del></span>`,
+//         btn: "Add to cart",
+//         p4: "Share",
+//         p5: "Compare",
+//         p6: "Like"
+//     },
+//     {
+//         images: "./images/Images (2).png",
+//         p1: "Potty",
+//         p2: "Minimalist flower pot",
+//         p3: "Rp 500.000",
+//         btn: "Add to cart",
+//         p4: "Share",
+//         p5: "Compare",
+//         p6: "Like"
+//     }
+// ]
+let data = [
     {
-        images: "./images/Images.png",
-        p1: "Syltherine",
-        p2: "Stylish cafe chair",
-        p3: `Rp 2.500.000 <span class="opacity-50"><del> Rp 3.500.000</del></span>`,
-        btn: "Add to cart",
-        p4: "Share",
-        p5: "Compare",
-        p6: "Like"
+      images:"./images/Images.png",
+      brand: "Syltherine",
+      caterogy: "Stylish cafe chair",
+      discountprice: "2.500.000",
+      totalprice: "3.500.000",
+      discountpersent: "-30%"
     },
     {
-        images: "./images/Images (3).png",
-        p1: "Leviosa",
-        p2: "Stylish cafe chair",
-        p3: "Rp 2.500.000",
-        btn: "Add to cart",
-        p4: "Share",
-        p5: "Compare",
-        p6: "Like"
+      images: "./images/Images (3).png",
+      brand: "Leviosa",
+      caterogy: "Stylish cafe chair",
+      discountprice: "2.500000",
+      discountpersent: "New"
+  
     },
     {
-        images: "./images/Images (1).png",
-        p1: "Lolito",
-        p2: "Luxury big sofa",
-        p3: `Rp 7.000.000 <span class="opacity-50"><del> Rp 14.000.000</del></span>`,
-        btn: "Add to cart",
-        p4: "Share",
-        p5: "Compare",
-        p6: "Like"
+      images:  "./images/Images (1).png",
+      brand: "Lolito",
+      caterogy: "Luxury big sofa",
+      discountprice: "Rp 7.000.000",
+      totalprice: "14.000.000",
+      discountpersent: "-50%"
+  
     },
     {
-        images: "./images/Images (2).png",
-        p1: "Respira",
-        p2: "Outdoor bar table and stool",
-        p3: "Rp 500.000",
-        btn: "Add to cart",
-        p4: "Share",
-        p5: "Compare",
-        p6: "Like"
+      images: "./images/Images (2).png",
+      brand: "Syltherine",
+      caterogy: "Stylish cafe chair",
+      discountprice: "2.500000",
+      totalprice: "3.500000",
+      discountpersent: "New"
+  
     },
     {
-        images: "./images/Images (4).png",
-        p1: "Grifo",
-        p2: "Night lamp",
-        p3: "Rp 1.500.000",
-        btn: "Add to cart",
-        p4: "Share",
-        p5: "Compare",
-        p6: "Like"
+      images: "./images/Images (4).png",
+      brand: "Grifo",
+      caterogy: "Night lamp",
+      discountprice: "1.500.000",
+      discountpersent: "New"
     },
     {
-        images: "./images/Images (2).png",
-        p1: "Muggo",
-        p2: "Small mug",
-        p3: "Rp 150.000",
-        btn: "Add to cart",
-        p4: "Share",
-        p5: "Compare",
-        p6: "Like"
+      images: "./images/Images (2).png",
+      brand: "Muggo",
+      caterogy: "Small mug",
+      discountprice: "150.000",
+      discountpersent: "New"
+  
     },
     {
-        images: "./images/Images.png",
-        p1: "Pingky",
-        p2: "Cute bed set",
-        p3: `Rp 7.000.000 <span class="opacity-50"><del>Rp 14.000.000</del></span>`,
-        btn: "Add to cart",
-        p4: "Share",
-        p5: "Compare",
-        p6: "Like"
+      images:"./images/Images.png",
+      brand: "Pingky",
+      caterogy: "Cute bed set",
+      discountprice: "7.000.000",
+      totalprice: "14.000.000",
+      discountpersent: "-50%"
+  
     },
     {
-        images: "./images/Images (2).png",
-        p1: "Potty",
-        p2: "Minimalist flower pot",
-        p3: "Rp 500.000",
-        btn: "Add to cart",
-        p4: "Share",
-        p5: "Compare",
-        p6: "Like"
+      images:  "./images/Images (2).png",
+      brand: "Potty",
+      caterogy: "Minimalist flower pot",
+      discountprice: "500.000",
+      discountpersent: "New"
     }
-]
-
-let main_row = document.getElementById("div-row")
-function getData() {
-    datas.map((values) => {
-        main_row.innerHTML += `
-                        <div class="col-lg-3 col-md-4 col-sm-6 mt-3">
-                            <div class="bg-body-secondary mainorder position-relative overflow-hidden"
-                                style="height: 100%;">
-                                <img width="100%" height="70%" style="object-fit: cover;"
-                                    src="${values.images}" alt="">
-                                <div class="positions">
-                                    <p class="up p-4">-30%</p>
-                                </div>
-                                <div class="p-3 mediatext">
-                                    <p class="fw-bold fs-5" style="margin-top: -10px;">${values.p1}</p>
-                                    <p class="opacity-75" style="margin-top: -10px;">${values.p2}</p>
-                                    <p class="fw-bold" style="margin-top: -10px;">${values.p3}</p>
-                                </div>
-                                <div class="topposition p-4">
-                                    <div class="text-center ">
-                                        <a href="http://127.0.0.1:5501/HTML/product.html">
-                                            <button class="upcart"> ${values.btn}</button>
-                                        </a>
-                                        <div class="d-flex gap-3 text-light mt-3">
-                                            <p><i class="fa-solid fa-share-nodes"></i> ${values.p4}</p>
-                                            <p><i class="fa-solid fa-arrow-right-arrow-left"></i> ${values.p5}</p>
-                                            <p><i class="fa-regular fa-heart"></i> ${values.p6}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+  
+  ]
+  
+  // function addproducts(img,brands,caterogys,totprice,disprice){
+  //   let mainrow = document.getElementById("loder-row");
+  //   let maindiv = document.createElement("div")
+  //   maindiv.classList.add("col-sm-12","col-md-6", "col-lg-3", "mt-3")
+  
+  //   let childdiv = document.createElement("div")
+  
+  //   let images = document.createElement("img")
+  //   images.src = img
+  //   images.classList.add("w-100")
+  //   childdiv.appendChild(images)
+  
+  //   let brand = document.createElement("h5")
+  //   brand.innerText = brands
+  //   childdiv.appendChild(brand)
+  
+  //   let categroy = document.createElement("p")
+  //   categroy.innerText = caterogys
+  //   childdiv.appendChild(categroy)
+  
+  //   let discountprice = document.createElement("h6")
+  //   discountprice.innerText = totprice
+  //   childdiv.appendChild(discountprice)
+  
+  //   let totalprice = document.createElement("p")
+  //   totalprice.innerText = disprice
+  //   totalprice.style.textDecoration="line-through"
+  //   childdiv.appendChild(totalprice)
+  
+  //   maindiv.appendChild(childdiv)
+  //   mainrow.appendChild(maindiv)
+  // }
+  
+  function homeproducts() {
+    setTimeout(() => {
+      let mainrow = document.getElementById("loder-row")
+      mainrow.innerHTML = " "
+  
+      data.map((value, key) => {
+        let mainrow = document.getElementById("loder-row")
+        mainrow.innerHTML += `
+          <div class="col-sm-12 col-md-6 col-lg-3 mt-3 ">
+                <div class="p-1">
+                 <a href="/single pruducts/products.html" class="main-for-anker-tag"> <div class="main-for-cart">
+                  <div class="position-relative h-100">
+                    <img width="100%" class="imgahes-div-js" src="${value.images}" alt="">
+                     <div class="abs-div-img">
+                    <p class="pt-3">${value.discountpersent ? value.discountpersent : " "}</p>
+                  </div>
+                  </div>
+                 <div class="ps-2">
+                  <h5 class="pt-2">${value.brand}</h5>
+                  <p class="opacity-75">${value.caterogy}</p>
+                  <div class="d-flex gap-2 align-items-center img-detailes">
+                    <p class="fw-bolder">${value.discountprice}</p>
+                    <p class="opacity-75 linethrow">${value.totalprice ? value.totalprice : " "}</p>
+                  </div>
+                 </div>
+                  <div class="abs-for-blur-child">
+                   <div class="mt-5 pt-5">
+                    <div class="mt-5">
+                      <button class="btn btn-light ps-4 pe-4 ms-3 text-warning">Add to cart</button>
+                      <div class="d-flex  align-items-center gap-3">
+                        <div class="d-flex align-items-center gap-1 ms-2">
+                          <i class="fa-solid fa-share-nodes" style="color: #f5f5f5;"></i>
+                          <p class="text-light pt-3">Share</p>
                         </div>
-                    `
-    })
-}
-getData();
-function placeHolder() {
-    [1, 2, 3, 4, 5, 6, 7, 8].map(() => {
-        let main_row = document.getElementById("main-row");
-        let main_div = document.createElement("div")
-        main_div.classList.add("col-sm-6", "col-lg-3", "col-md-4", "mt-3");
-        main_row.appendChild(main_div)
-        let card = document.createElement("div");
-        card.classList.add("card")
-        card.ariaHidden = "true"
-        main_div.appendChild(card);
-        let card_body = document.createElement("div");
-        card_body.classList.add("card-body")
-        card.appendChild(card_body);
-        let h5 = document.createElement("h5");
-        h5.classList.add("card-title", "placeholder-glow");
-        card_body.appendChild(h5);
-        let span = document.createElement("span");
-        span.classList.add("placeholder", "col-6", "w-100");
-        span.style.height = "200px";
-        h5.appendChild(span);
-        let p = document.createElement("p");
-        p.classList.add("card-text", "placeholder-glow", "d-flex", "gap-3")
-        p.style.flexDirection = "column";
-        card_body.appendChild(p);
-        let span1 = document.createElement("span");
-        span1.classList.add("placeholder", "col-5", "mt-3");
-        span1.style.height = "20px";
-        let span2 = document.createElement("span");
-        span2.classList.add("placeholder", "col-6");
-        span2.style.height = "20px";
-        let span3 = document.createElement("span");
-        span3.classList.add("placeholder", "col-7");
-        span3.style.height = "20px";
-        p.appendChild(span1);
-        p.appendChild(span2);
-        p.appendChild(span3);
-    })
-};
-placeHolder();
-
-setTimeout(() => {
-    let divRow = document.getElementById("div-row");
-    divRow.style.display = "none"
-}, 0);
-
-setTimeout(() => {
-    let mainRow = document.getElementById("main-row");
-    mainRow.style.display = "none"
-    let divRow = document.getElementById("div-row");
-    divRow.style.display = "flex"
-}, 3000);
+                        <div class="d-flex align-items-center gap-1">
+                          <i class="fa-solid fa-code-compare" style="color: #ffffff;"></i>
+                            <p class="text-light pt-3">compare</p>
+                        </div>
+                        <div class="d-flex align-items-center gap-1">
+                          <i class="fa-regular fa-heart" style="color: #ffffff;"></i>
+                              <p class="text-light pt-3">Like</p>
+                        </div>
+                      </div>
+                    </div>
+                   </div>
+                  </div>
+                </div></a>
+                </div>
+                </div>
+        `
+      });
+    }, 2000);
+  }
+  homeproducts();
+  
