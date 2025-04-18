5d98f8c491ecd858839c70b218f3ec7bf66df258
rahul-yadav695 <officialrahulyadav961@gmail.com>
2025-04-18 14:26:20 +0530

    new code puch

--- Contact/contact.js
+++ Contact/contact.js
@@ -1,4 +1,23 @@
 let counter = 0; 
 let ValueCount = document.getElementById("plus1")
 
-let one = document.getElementById("")
+let one = document.getElementById("increase")
+one.addEventListener('click' , function () {
+    counter++
+    ValueCount.textContent = counter
+})
+
+const two = document.getElementById('decrease')
+two.addEventListener('click' , function () {
+    if(counter > 0){
+        counter--;
+    }
+    ValueCount.textContent = counter
+})
+
+
+const raj = document.getElementById('rahulk')
+raj.addEventListener('click' , function () {
+    raj.innerHTML = `
+    `
+})
